6b02961527c0fc8fa2f6850f484d5c06d956ffd4
Ayandashozi81 <ayandashozi81@gmail.com>
2025-04-23 17:24:00 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+let searchForm = document.querySelector('.search-form');
+
+document.querySelector('#search-btn').onclick = () =>{
+    searchForm.classList.toggle('active');
+}
+
+let shoppingCart = document.querySelector('.search-form');
+
+document.querySelector('#cart-btn').onclick = () =>{
+    shoppingCart.classList.toggle('active');
+}
